4c55814a029de553f038898bd06b4f524a5cae4c
Elton Ribeiro <ton.rc@live.com>
2019-10-04 10:20:39 -0300

    Fim da aula 23 - memory game

---
+++ src/components/memory-card/index.js
@@ -0,0 +1,31 @@
+function createMemoryCard() {
+    const $memoryCard = document.createElement("article");
+
+    const $iconCollab = `
+    <img 
+        src='img/icon-collabcode.png' 
+        alt='Gueio mascote da CollabCode' 
+        class='icon'
+    >`;
+
+    $memoryCard.classList.add("memory-card");
+    $wrapCards.insertBefore($memoryCard, null);
+
+    $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
+
+function createMemoryCardFront() {
+    const $memoryCardFront = document.createElement("article");
+    const $iconC = `
+<img 
+    src='img/icon-c.png' 
+    alt='Ícone de um livro da linguagem C++' 
+    class='icon'
+>`;
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$wrapCards.insertBefore($memoryCardFront, null);
+
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+}
---
+++ src/pages/game/index.js
@@ -0,0 +1,9 @@
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCardFront();
+createMemoryCardFront();
+
