6b94377f3275ee479f9625053f8d24c6bfa5e7d7
Elton Ribeiro <ton.rc@live.com>
2019-10-24 21:49:42 -0300

    Fim da aula 33

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,32 +2,28 @@
     const $root = document.querySelector("#root");
 
     const $cardsWrapper = createCardsWrapper();
-
     const createMemoryCard = memoryCard.create();
+
+
+    const $pointBar = pointBar.create();
 
     const $memoryCardC = createMemoryCard({
         src: "img/icon-c.png",
         alt: "Ícone de um livro da linguagem C++"
     });
-
     const $memoryCardJS = createMemoryCard({
         src: "img/icon-js.png",
         alt: "Ícone de um livro da linguagem JavaScript"
     });
-
     const $memoryCardJava = createMemoryCard({
         src: "img/icon-java.png",
         alt: "Ícone de um livro da linguagem Java"
     });
-
     const $memoryCardWoman = createMemoryCard({
         src: "img/icon-woman.png",
         alt: "Ícone de uma mulher programando"
     });
 
-
-
-
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
@@ -37,8 +33,7 @@
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
 
-
-
+    $root.insertAdjacentHTML("afterbegin", $pointBar);
     $root.insertAdjacentElement("beforeend", $cardsWrapper);
 
 })();
