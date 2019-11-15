da948cc7bfe8a5c31d775f403a8a7da29596b3ef
Elton Ribeiro <ton.rc@live.com>
2019-11-15 12:15:32 -0200

    Fim da aula 40

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,9 +1,14 @@
 (function () {
     const $root = document.querySelector("#root");
 
-    const $loginButton = flatButton.render("Log in");
+    const $loginButton = flatButton.render("Log in", false);
     const $signupButton = flatButton.render("Sign up", true);
+    const $logoCollabcode = logoCollabcode.render();
+    const $titleCollabcode = titleCollabcode.render("Welcome!");
+
+    const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
 
     $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signupButton);
+    $root.insertAdjacentHTML("beforeend", $logoWrapper);
 })();
