85238c660c042972d0ac2e9abe13dbda7754f6c3
Elton Ribeiro <ton.rc@live.com>
2019-11-20 16:47:17 -0200

    043 02 sem ordem nos parâmetros

--- src/objetcs/input-collabcode/index.js
+++ src/objetcs/input-collabcode/index.js
@@ -21,8 +21,9 @@ const inputCollabcode = (function () {
         $head.insertAdjacentElement("beforeend", $style);
     }
 
-    module.render = (placeholder= "", type= "text") => {
+    module.render = ({ placeholder = "", type = "text"}) => {
         module._style();
+
         return `<input class="input-collabcode" type="${type}" placeholder=${placeholder}>`
     };
     return {
