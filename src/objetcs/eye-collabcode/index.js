15d41e76db5017b70c74aaa611659ce7a2df69dc
Elton Ribeiro <ton.rc@live.com>
2019-11-21 23:00:14 -0200

    Adicionou a imagem eye.png por CSS

--- src/objetcs/eye-collabcode/index.js
+++ src/objetcs/eye-collabcode/index.js
@@ -2,13 +2,30 @@ const eyeCollabcode = (function() {
     const module = {};
 
     module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
 
+        $style.textContent = `
+            .eye-collabcode {
+                background-image: url(/img/eye.png);
+                background-repeat: no-repeat;
+                background-position: center;
+                display: block;
+                text-indent: -9999px;
+                width: 24px;
+                height: 15px;
+            }
+        `;
+
+        $head.insertAdjacentElement("beforeend", $style);
     };
 
-    module.render = () => `
-        <label>Mostrar Senha</label>
+    module.render = () => {
+        module._style();
+        return `
+        <label class="eye-collabcode">Mostrar Senha</label>
     `;
-
+};
     return {
         render: module.render
     }
